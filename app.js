7a869eb7b4a7d56a4d5403971999ea94f9b6b2e7
Chihiro88G <1114chihiro@gmail.com>
2024-08-29 18:35:07 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log('in the middleware');
+})
+
+app.use((req, res, next) => {
+  console.log('in the another middleware');
+})
+
+const server = http.createServer(app);
+
+server.listen(3300);
