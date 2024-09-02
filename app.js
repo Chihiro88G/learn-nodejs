0b218a1ee689948d703afa71efe5b4b52212a6c6
Chihiro88G <1114chihiro@gmail.com>
2024-09-02 15:46:08 -0400

    next()

--- app.js
+++ app.js
@@ -4,14 +4,30 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('in the middleware');
+// path to different middleware
+app.use('/different-path', (req, res, next) => {
+  console.log('in another middleware');
+  res.send('<p>LEARNING EXPRESS</p>');
 })
 
-app.use((req, res, next) => {
-  console.log('in the another middleware');
+app.use('/', (req, res, next) => {
+  console.log('in another middleware');
+  res.send('<p>LEARNING EXPRESS</p>');
 })
 
 const server = http.createServer(app);
 
 server.listen(3300);
+
+
+// ****************************************************************** //
+// app.use((req, res, next) => {
+//   console.log('in the middleware');
+//   next(); // allows to continue to the next middleware (the one below)
+// })
+
+// app.use((req, res, next) => {
+//   console.log('in another middleware');
+//   res.send('<p>LEARNING EXPRESS</p>');
+// })
+// ****************************************************************** //
